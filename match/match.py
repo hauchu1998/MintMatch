7a118858ccdc8eea711dc5de47f06110febe9ccf
Maxim <maximclouser@gmail.com>
2023-09-22 22:45:13 -0400

    fixed matching

--- match/match.py
+++ match/match.py
@@ -1,16 +1,25 @@
 from typing import List
-import heapq
+from queue import PriorityQueue
 
-def findMatches(user_profiles: List[List[int]]) -> List[int]:
-  priority_queue = []
+def findMatches(user_to_match: List, user_profiles: List[List[int]]) -> List[int]:
+  priority_queue = PriorityQueue()
 
   for user in user_profiles:
-    priority_rank = -1 * sum(user)
-    heapq.heappush(priority_queue,((priority_rank, user)))
+    priority_rank = 0
+    for label1, label2 in zip(user_to_match, user):
+      priority_rank += label1 * label2 
+      if label1 != label2: #penalize difference
+        priority_rank -= 1
 
-  matched_users = [user for _, user in priority_queue]
-  return matched_users
+    priority_queue.put((priority_rank, user))
 
-users = [[1,0,1],[0,0,1],[1,1,1]]
-p = findMatches(users)
-print(p)
+  matches_users = []
+  while not priority_queue.empty(): #to list
+    matches_users.append(priority_queue.get()[1])
+  return matches_users[::-1] #rev order
+  
+
+# user = [0,0,1]
+# users = [[1,0,1],[0,0,1],[1,1,1],[0,0,0],[1,1,1],[0,1,1],[0,1,0]]
+# p = findMatches(user, users)
+# print(p)
