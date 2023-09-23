b9f370efd4c1a24d16531e11c4877b553713437c
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-09-22 23:41:11 -0400

    Minor changes with optimization and remove unused imports

--- hooks/useGetUserMessage.ts
+++ hooks/useGetUserMessage.ts
@@ -1,84 +1,52 @@
 import { ConversationV2, Message, Stream } from '@xmtp/xmtp-js';
 import { useCallback, useEffect, useState } from 'react';
-
 import { XmtpContext } from '../contexts/XmtpContexts';
 import useXmtp from './useXmtp';
 
 type OnMessageCallback = () => void;
 
-const useGetUserMessage = (
-  peerAddress: string,
-  onMessageCallback?: OnMessageCallback
-) => {
+const useGetUserMessage = (peerAddress: string, onMessageCallback?: OnMessageCallback) => {
   const { client, getMessages, dispatchMessages } = useXmtp();
   const [conversation, setConversation] = useState<ConversationV2<any> | null>(null);
-  const [stream, setStream] = useState<Stream<any>>();
   const [loading, setLoading] = useState<boolean>(false);
-  useEffect(() => {
-    const getConvo = async () => {
-      if (!client) {
-        return;
+
+  const handleMessages = useCallback(
+    async (messages: Message[], shouldDispatch = true) => {
+      if (shouldDispatch && dispatchMessages) {
+        dispatchMessages({ peerAddress, messages });
       }
-      setConversation(await client.conversations.newConversation(peerAddress) as ConversationV2<String>);
-    };
-    getConvo();
-  }, [client, peerAddress]);
-
-  useEffect(() => {
-    const closeStream = async () => {
-      if (!stream) return;
-      await stream.return();
-    };
-    closeStream();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [peerAddress]);
-
-  useEffect(() => {
-    const listMessages = async () => {
-      if (!conversation) return;
-
-      setLoading(true);
-      const msgs = await conversation.messages({ limit: 100 });
-      if (dispatchMessages) {
-        dispatchMessages({
-          peerAddress: conversation.peerAddress,
-          messages: msgs as unknown as Message[],
-        });
-      }
-
       if (onMessageCallback) {
         onMessageCallback();
       }
-      setLoading(false);
-    };
-    listMessages();
-  }, [conversation, dispatchMessages, onMessageCallback, setLoading]);
-  
-  useEffect(() => {
-    const streamMessages = async () => {
-      if (!conversation) return;
-      const stream = await conversation.streamMessages();
-      setStream(stream);
-      for await (const msg of stream) {
-        if (dispatchMessages) {
-          dispatchMessages({
-            peerAddress: conversation.peerAddress,
-            messages: [msg as unknown as Message],
-          });
-        }
+    },
+    [dispatchMessages, onMessageCallback, peerAddress]
+  );
 
-        if (onMessageCallback) {
-          onMessageCallback();
+  useEffect(() => {
+    const initialize = async () => {
+      if (client) {
+        const convo = await client.conversations.newConversation(peerAddress) as ConversationV2<String>;
+        setConversation(convo);
+        const messages = await convo.messages({ limit: 100 });
+        handleMessages(messages as unknown as Message[]);
+        setLoading(false);
+
+        const stream = await convo.streamMessages();
+        for await (const msg of stream) {
+          handleMessages([msg as unknown as Message]);
         }
       }
     };
-    streamMessages();
-  }, [conversation, peerAddress, dispatchMessages, onMessageCallback]);
+
+    setLoading(true);
+    initialize();
+  }, [client, handleMessages, peerAddress]);
 
   const handleSend = useCallback(
     async (message: string) => {
-      if (!conversation) return;
-      await conversation.send(message);
+      if (conversation) {
+        await conversation.send(message);
+      }
     },
     [conversation]
   );
--- hooks/useMessageStore.ts
+++ hooks/useMessageStore.ts
@@ -1,24 +1,26 @@
 import { Message } from '@xmtp/xmtp-js';
 import { useCallback, useReducer } from 'react';
-
 import { MessageStoreEvent } from '../contexts/XmtpContexts';
 
 type MessageDeduper = (message: Message) => boolean;
 type MessageStore = { [address: string]: Message[] };
 
 const buildMessageDeduper = (state: Message[]): MessageDeduper => {
-    const existingMessageKeys = state.map((msg) => msg.id);
-    return (msg: Message) => existingMessageKeys.indexOf(msg.id) === -1
+  const existingMessageKeys = new Set(state.map((msg) => msg.id));
+  return (msg: Message) => !existingMessageKeys.has(msg.id);
 };
 
 const useMessageStore = () => {
   const [messageStore, dispatchMessages] = useReducer(
     (state: MessageStore, { peerAddress, messages }: MessageStoreEvent) => {
       const existing = state[peerAddress] || [];
-      const newMessages = messages.filter(buildMessageDeduper(existing));
+      const deduper = buildMessageDeduper(existing);
+      const newMessages = messages.filter(deduper);
+
       if (!newMessages.length) {
         return state;
       }
+
       console.log('Dispatching new messages for peer address', peerAddress);
 
       return {
